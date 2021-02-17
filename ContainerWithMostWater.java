d7d74681173da4379d5c08e47d2d7ba5cb51a3d9
Tatiana Belousova <t.belousova@gmail.com>
2021-02-17 00:26:33 -0600

    added file ContainerWithMostWater.java LC #11

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+//11. Container With Most Water
+class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.length-1;
+        while (left<right) {
+            int vol = Math.min(height[left],height[right])*(right-left);
+            ans = Math.max(ans, vol);
+            if (height[left]<height[right]){
+                left++;
+            } else {
+                right--;
+            }
+        
+        }
+        return ans;
+    }
+}
