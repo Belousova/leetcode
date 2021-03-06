f4fd5528e3971f122ec8dd549d2c36af46430d80
Tatiana Belousova <t.belousova@gmail.com>
2021-03-06 17:48:01 -0600

    added file InvertBinaryTree.java LC #226

---
+++ InvertBinaryTree.java
@@ -0,0 +1,25 @@
+//226. Invert Binary Tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root==null) return null;
+        TreeNode node = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(node);
+        return root;
+    }
+}
