49653a42d9f4f3963528374581f6adb946b00520
Tatiana Belousova <t.belousova@gmail.com>
2021-04-24 15:15:31 -0500

    added file MinimumCostForTickets.java LC #983

---
+++ MinimumCostForTickets.java
@@ -0,0 +1,31 @@
+//983. Minimum Cost For Tickets
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+ 
+        int dp[] = new int[days[days.length-1]+1];
+        int last = 0;
+        for (int i=1; i< dp.length; i++) {
+            if (i < days[last]) {
+                if (i>1) {
+                    dp[i] = dp[i-1];
+                } else {
+                    dp[i] = 0;
+                }
+            } 
+            if (i==days[last]) {
+                //travel this day
+                last ++;
+                if (i>30) {
+                    dp[i] = Math.min(dp[i-1] + costs[0], 
+                                Math.min(costs[1] + dp[i-7], costs[2] + dp[i-30]));
+                } else if (i>7) {
+                    dp[i] = Math.min(dp[i-1] + costs[0], 
+                                     Math.min(costs[1] + dp[i-7], costs[2]));
+                } else {
+                    dp[i] = Math.min(dp[i-1] + costs[0], Math.min(costs[1],costs[2]));
+                }
+            } 
+        }
+        return dp[dp.length-1];
+    }       
+}
