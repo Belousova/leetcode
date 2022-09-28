e340a0b264be8442df28b4e7bfd25c481ecc5512
Tatiana Belousova <t.belousova@gmail.com>
2022-09-28 14:52:34 -0500

    added file SmallestEvenMultiple.java LC #2413

---
+++ SmallestEvenMultiple.java
@@ -0,0 +1,10 @@
+//2413. Smallest Even Multiple
+class Solution {
+    public int smallestEvenMultiple(int n) {
+      if (n%2 == 0) {
+          return n;
+      } else {
+          return n*2;
+      }
+    }
+}
